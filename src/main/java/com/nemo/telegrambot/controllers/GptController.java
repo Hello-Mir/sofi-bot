8a182141dd8eab8d7757c43b3857dfe5078039f8
justW4n <justW4n@mail.ru>
2023-07-02 21:43:38 +0300

    start writing controller

---
+++ src/main/java/com/nemo/telegrambot/controllers/GptController.java
@@ -0,0 +1,10 @@
+package com.nemo.telegrambot.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/openai/...")
+public class GptController {
+
+}
