ae64bbe1db9bdb3335dd9d73e4eeba74edc8448b
t.sawyer <yessss42@gmail.com>
2023-07-21 15:28:18 +0300

    wip init tables

--- src/main/java/com/nemo/telegrambot/database/connection/InitDbTables.java
+++ src/main/java/com/nemo/telegrambot/database/connection/InitDbTables.java
@@ -1,6 +1,9 @@
 package com.nemo.telegrambot.database.connection;
 
 import org.springframework.stereotype.Component;
+import org.sql2o.Connection;
+import org.sql2o.Query;
+import org.sql2o.Sql2o;
 
 @Component
 public class InitDbTables {
@@ -8,6 +11,39 @@ public class InitDbTables {
 
     public InitDbTables(Sql2OConnect sql2OConnect) {
         this.sql2OConnect = sql2OConnect;
+    }
+
+    public void initTables() {
+        Sql2o connection = sql2OConnect.createSql();
+        try (Connection open = connection.open()) {
+            createUserTable(open);
+            createUserRequests(open);
+        }
+    }
+
+    private void createUserTable(Connection connection) {
+        String queryForUserTable = """
+                create table IF NOT EXISTS users
+                (
+                    id bigint not null primary key,
+                    msg_number integer not null,
+                    name varchar(255)
+                )
+                """;
+        Query query = connection.createQuery(queryForUserTable);
+        query.executeUpdate();
+    }
+
+    private void createUserRequests(Connection connection) {
+        String queryText = """
+                    create table IF NOT EXISTS user_requests
+                    (id serial primary key, conversation_id varchar(50), user_id bigint
+                    constraint fk_user_request_user references users on delete cascade,
+                    created_at timestamp
+                    )
+                """;
+        Query query = connection.createQuery(queryText);
+        query.executeUpdate();
     }
 
 }
